4d975e680ad2a302ed7cef8807ff2630fe3bcea4
farrelfauzan <farrelfauzan78@gmail.com>
2022-05-17 22:34:56 +0700

    debug formAdd

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.set("view engine", "ejs")
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '/public'));
 
 app.use(router)
 
